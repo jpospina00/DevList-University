41e02f00861866c907acd8d1a28a06bc65731eee
Juan Pablo <juan.ospina.vanegas@correounivalle.edu.co>
2024-09-08 12:01:50 -0500

    feat: Add configuration file for environment variables and update package.json scripts

---
+++ src/config/config.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+
+const env = process.env.NODE_ENV || 'dev';
+
+// Cargar el archivo .env correspondiente
+dotenv.config({ path: `./envs/.env.${env}` });
+
+export const config = {
+  env: process.env.NODE_ENV,
+  port: process.env.PORT || 3000,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST,
+  dbName: process.env.DB_NAME,
+  dbPort: process.env.DB_PORT,
+};
