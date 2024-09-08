c8955c29c8612dbb2ab549d7799e39e9f0a12040
Juan Pablo <juan.ospina.vanegas@correounivalle.edu.co>
2024-09-08 12:41:07 -0500

    Refactor: Update user.route.js to use UserService for user-related operations

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,18 +1,28 @@
-import express from 'express';
+import express from "express";
+import UserService from "../services/user.service.js";
 
 const router = express.Router();
+const userService = new UserService();
 
-router.post('/login', (req, res) => {
-    try {
-        const { email, password } = req.body;
-        if(email === 'test@gmai.co' && password === '123456') {
-            res.status(200).json({ message: 'Login successful' });
-        } else {
-            res.status(401).json({ message: 'Invalid credentials' });
-        }
-    } catch (error) {
-        console.error(error);
-    }
+router.get("/", async (req, res) => {
+  try {
+    const users = await userService.getAllUsers();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userAuthenticated = await userService.getUserByEmail(email);
+    console.log(userAuthenticated.dataValues);
+    res.status(200).json({ message: "Login successful" });
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({ message: "Invalid credentials" });
+  }
 });
 
 export default router;
