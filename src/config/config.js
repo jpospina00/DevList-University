51c90f6014c3ab3ce49bf0e67201c51e6765f466
Juan Pablo <juan.ospina.vanegas@correounivalle.edu.co>
2024-09-14 11:54:36 -0500

    feat: Add jsonwebtoken package for token generation

--- src/config/config.js
+++ src/config/config.js
@@ -13,4 +13,5 @@ export const config = {
   dbHost: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
   dbPort: process.env.DB_PORT,
+  jwtSecret: process.env.JWT_SECRET,
 };
---
+++ src/middlewares/auth.handler.js
@@ -0,0 +1,20 @@
+import jwt from 'jsonwebtoken';
+import { config } from '../config/config.js';
+
+const authenticateToken = (req, res, next) => {
+  const token = req.headers['authorization'];
+
+  if (!token) {
+    return res.status(403).json({ message: 'Token not provided' });
+  }
+
+  try {
+    const verified = jwt.verify(token.split(' ')[1], config.jwtSecret); // Extrae el token del header
+    req.user = verified; // Almacena los datos del usuario en `req.user`
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+};
+
+export default authenticateToken;
