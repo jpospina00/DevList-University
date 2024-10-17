576f9becaab138c409b6eb8a1ba947eb46d06b06
GustavoLS <106133669+GustavoLS1@users.noreply.github.com>
2024-10-17 17:00:31 -0500

    add inputs and buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
     return(
         <header className="bg-transparent w-screen h-[100px] absolute top-0 z-50 flex px-20 items-center gap-10">{/**Colocar la imagen en los assets, importarla aca */}
           <img src={logo} width={"90px"} height={"94px"}/>  
-          <h1 className="text-secondary1 font-semibold text-[36px] font-montserrat w-[428px] h-[44px]">DevList University</h1>
+          <h1 className="text-secondary0 font-semibold text-[36px] font-montserrat w-[428px] h-[44px]">DevList University</h1>
         </header>
     )
 }
--- src/components/UserRegister.jsx
+++ src/components/UserRegister.jsx
@@ -4,20 +4,117 @@ import IconUserRegister from "../assets/IconUserRegister.svg";
 export default function UserRegister() {
   return (
     <>
-      <div className="flex flex-col justify-start items-center h-screen pt-20">
+      <div className="flex flex-col justify-start items-center h-screen pt-20 bg-disable">
         <Header />
         <div className="flex flex-col justify-center items-center mt-10">
-          <h1 className="font-bold text-center text-2xl mb-4 ">
+          <h1 className="flex font-bold text-left text-2xl  mr-[1150px] items-center justify-center">
             Registre el Usuario
           </h1>
-        </div>
-        <div>
-          <div>
-            <img className="w-24 h-24 mb-8" src={IconUserRegister}></img>
-            <div className="flex justify-start font-bold">
-              <p className="text-left w-[500px]">Nombre</p>
+          <div className="mb-4">
+            <div className="flex flex-col justify-center items-center w-32 h-36 mr-[1150px] ">
+              <img src={IconUserRegister}></img>
             </div>
           </div>
+        </div>
+        {/* Input para Nombre */}
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2 -ml-[880px] "
+            htmlFor="name"
+          >
+            Nombre
+          </label>
+          <input
+            id="name"
+            type="text"
+            placeholder="Ingrese su nombre"
+            className="shadow appearance-none border rounded w-[600px] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline -ml-[880px]"
+          />
+        </div>
+
+        {/* Input para Correo Electrónico */}
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2 -ml-[880px]"
+            htmlFor="email"
+          >
+            Correo Electrónico
+          </label>
+          <input
+            id="email"
+            type="email"
+            placeholder="Ingrese su correo electrónico"
+            className="shadow appearance-none border rounded w-[600px] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline -ml-[880px]"
+          />
+        </div>
+
+          {/* Input para Número de Teléfono */}
+          <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2 -ml-[880px]" htmlFor="phone" >
+            Número de Teléfono
+          </label>
+          <input
+            id="phone"
+            type="tel"
+            placeholder="Ingrese su número de teléfono"
+            className="shadow appearance-none border rounded w-[600px] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline -ml-[880px]"
+          />
+        </div>
+
+            {/* Input para Dirección */}
+            <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2 -ml-[880px]" htmlFor="address">
+            Dirección
+          </label>
+          <input
+            id="address"
+            type="text"
+            placeholder="Ingrese su dirección"
+            className="shadow appearance-none border rounded w-[600px] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline -ml-[880px]"
+          />
+        </div>
+
+           {/* Input para Contraseña */}
+           <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2 -ml-[880px]" htmlFor="password">
+            Contraseña
+          </label>
+          <input
+            id="password"
+            type="password"
+            placeholder="Ingrese su contraseña"
+            className="shadow appearance-none border rounded w-[600px] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline -ml-[880px]"
+          />
+        </div>
+
+             {/* Input para Confirmar Contraseña */}
+             <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2 -ml-[880px]" htmlFor="confirmPassword">
+            Confirmar Contraseña
+          </label>
+          <input
+            id="confirmPassword"
+            type="password"
+            placeholder="Confirme su contraseña"
+            className="shadow appearance-none border rounded w-[600px] py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline -ml-[880px]"
+          />
+        </div>
+
+        {/* Botones Guardar y Cancelar */}
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline -ml-[60px]"
+            type="submit"
+          >
+            Guardar
+          </button>
+          <button
+            className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+            // onClick={() => alert('Cancelar')}
+          >
+            Cancelar
+          </button>
         </div>
       </div>
     </>
