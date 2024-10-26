64bc6786e9d273cae9f7e5cd9e37ea69252b4a06
JALZ <jannus.largo@correounivalle.edu.co>
2024-10-26 00:10:39 -0500

    Success page "add device"

---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,48 @@
+export default function Dropdown({ selectedOption, list, handleOptionChange, setIsOpen, isOpen }) {
+    return (
+        <div className="relative cursor-pointer transition duration-300 z-20">
+            <div
+                className="border border-dark p-2 mb-1 rounded flex items-center justify-between"
+                onClick={() => setIsOpen(!isOpen)} // Alterna el estado del dropdown
+            >
+                <span>
+                    {selectedOption === 'all' ? 'All' : selectedOption}
+                </span>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="1em"
+                    viewBox="0 0 512 512"
+                    className={`transform transition duration-300 ${isOpen ? 'rotate-0' : 'rotate-[-90deg]'}`}
+                >
+                    <path
+                        d="M233.4 406.6c12.5 12.5 32.8 12.5 45.3 0l192-192c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L256 338.7 86.6 169.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l192 192z"
+                    />
+                </svg>
+            </div>
+            <div
+                className={`border border-dark flex gap-2 flex-col items-center rounded bg-disable absolute w-full transition-all duration-300 ${isOpen ? 'max-h-40 opacity-100 overflow-y-auto' : 'max-h-0 opacity-0 overflow-hidden'}`}
+            >
+                {list.map((option) => (
+                    <div key={option} title={option} className='w-[95%]'>
+                        <input
+                            id={option}
+                            name="option"
+                            type="radio"
+                            value={option}
+                            checked={selectedOption === option}
+                            onChange={handleOptionChange}
+                            className="hidden"
+                        />
+                        <label
+                            className={`cursor-pointer border border-dark block rounded p-1 transition duration-300 mb-2 w-full text-left hover:bg-primary0 ${selectedOption === option ? 'hidden' : ''
+                                }`}
+                            htmlFor={option}
+                        >
+                            {option === 'all' ? 'All' : option}
+                        </label>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
--- src/pages/FormAddDevice.jsx
+++ src/pages/FormAddDevice.jsx
@@ -1,67 +1,117 @@
 import { useRef, useState } from 'react'
 import imgAddDevice from '../assets/add-device-img.svg'
 import calendarIcon from '../assets/Calendar.svg';
+import { Link } from 'react-router-dom';
+import DatePicker from '../components/DatePicker';
+import Dropdown from '../components/Dropdown';
+import InputText from '../components/InputText';
 
 export default function FormAddDevice() {
 
-    const date = useRef();
+    const description = useRef();
+    const [count, setCount] = useState(0);
     const [day, setDay] = useState("");
     const [month, setMonth] = useState("");
     const [year, setYear] = useState("");
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [selectedOption, setSelectedOption] = useState('all');
+    const [isOpen, setIsOpen] = useState(false);
+    const [isOpenType, setIsOpenType] = useState(false);
+    const [selectedOptionType, setSelectedOptionType] = useState('all-1');
+    const [selectedDate, setSelectedDate] = useState(null);
+    const [showCalendar, setShowCalendar] = useState(false);
 
-    const getDate = () => {
-        let dateValue = date.current.value?.trim();
-        let values = dateValue.split("-");
-        if (values[0] == "") {
+    const getDate = (date) => {
+        if (date == null) {
             setYear("");
             setMonth("");
             setDay("");
         } else {
-            setYear(values[0]);
-            setMonth(values[1]);
-            setDay(values[2]);
+            setYear(date.getFullYear());
+            setMonth(date.getMonth() + 1);
+            setDay(date.getDate());
         }
+    }
+
+    const loadImage = (event) => {
+        setSelectedImage(event.target.files[0]);
+    }
+
+    const clickLoadImg = () => {
+        document.getElementById("loadImg").click()
+    }
+
+    const handleOptionChange = (event) => {
+        setSelectedOption(event.target.value);
+        setIsOpen(false); // Cierra el dropdown al seleccionar una opción
+    };
+
+    const handleOptionChangeType = (event) => {
+        setSelectedOptionType(event.target.value);
+        setIsOpenType(false); // Cierra el dropdown al seleccionar una opción
+    };
+
+    const getCharDescription = () => {
+        setCount(description.current.value?.trim().length);
     }
 
     return (
         <div className="h-screen w-full flex justify-around items-center">
             <div className='flex flex-col justify-center gap-7 items-center font-montserrat font-semibold w-2/4 h-full shadow-[0_35px_35px_35px_rgba(0,0,0,0.3)]'>
                 <h1 className='text-2xl'> Agregar un dispositivo </h1>
-                <div className='flex flex-col justify-around w-4/5 h-16'>
-                    <label> Nombre del dispositivo </label>
-                    <input className='h-7 pl-2 border border-dark rounded-lg hover:border-secondary0Hover outline-secondary0Hover' type="text" />
+                <div className='flex w-4/5 gap-5'>
+                    <InputText title={"N° de referencia"} />
+                    <div className='flex flex-col justify-around w-4/5 h-16'>
+                        <label> N° Bodega </label>
+                        <Dropdown list={['all', 'option-1', 'option-2', 'option-3']} handleOptionChange={handleOptionChange} selectedOption={selectedOption} setIsOpen={setIsOpen} isOpen={isOpen} />
+                    </div>
+                    <div className='flex flex-col justify-around w-4/5 h-16 '>
+                        <label> Tipo </label>
+                        <Dropdown list={['all-1', 'opcion 1', 'opcion 2', 'opcion 3']} handleOptionChange={handleOptionChangeType} selectedOption={selectedOptionType} isOpen={isOpenType} setIsOpen={setIsOpenType} />
+                    </div>
                 </div>
-                <div className='flex flex-col justify-around w-4/5 h-16'>
-                    <label> N° de referencia </label>
-                    <input className='h-7 pl-2 border border-dark rounded-lg hover:border-secondary0Hover outline-secondary0Hover' type="text" />
+                <div className='flex w-4/5 gap-5'>
+                    <InputText title={"Nombre del dispositivo"} />
+                    <InputText title={"Marca del dispositivo "} />
                 </div>
-                <div className='flex flex-col justify-around w-4/5 h-16'>
-                    <label> Tipo de dispositivo </label>
-                    <input className='h-7 pl-2 border border-dark rounded-lg hover:border-secondary0Hover outline-secondary0Hover' type="text" />
+                <div className='flex flex-col justify-around w-4/5 relative'>
+                    <label> Descripcion del dispositivo </label>
+                    <textarea ref={description}
+                        className={`text-start max-h-32 min-h-10 p-2 border border-dark rounded ${count > 250 ? "outline-[#ff2a2a]" : "outline-secondary0Hover"}`}
+                        onChange={getCharDescription}>
+                    </textarea>
+                    <p className='text-text text-opacity-30 absolute bottom-1 right-1'> {count}/250 </p>
+                    <p className={`text-[#ff2a2a] ${count > 250 ? "" : "hidden"}`}> Máximo de 250 carácteres </p>
                 </div>
+
                 <div className='flex flex-col justify-around w-4/5 h-16'>
-                    <label> N° Bodega </label>
-                    <input className='h-7 pl-2 border border-dark rounded-lg hover:border-secondary0Hover outline-secondary0Hover' type="text" />
-                </div>
-                <div className='flex flex-col justify-around w-4/5 h-16'>
-                    <label> N° Bodega </label>
-                    <div className='flex w-full justify-around items-center'>
-                        <input className='h-7 pl-2 w-[20%] border border-dark rounded-lg hover:border-secondary0Hover outline-secondary0Hover' type="text" placeholder='Día' value={day} readOnly />
-                        <input className='h-7 pl-2 w-[20%] border border-dark rounded-lg hover:border-secondary0Hover outline-secondary0Hover' type="text" placeholder='Mes' value={month} readOnly />
-                        <input className='h-7 pl-2 w-[20%] border border-dark rounded-lg hover:border-secondary0Hover outline-secondary0Hover' type="text" placeholder='Año' value={year} readOnly />
-                        <button className=' w-[20px] h-[25px] overflow-hidden cursor-pointer outline-none'>
-                            <input className='w-full h-full cursor-pointer' ref={date} onChange={() => getDate()} type="date" />
-                            {/* <img className='w-full h-full' src={calendarIcon} alt="Calendario" /> */}
+                    <label> Fecha </label>
+                    <div className='flex w-full justify-between items-center relative'>
+                        <input onClick={() => setShowCalendar(true)} required className='h-7 pl-2 w-[20%] border border-dark rounded-lg hover:border-secondary0Hover outline-secondary0Hover' placeholder='Dìa' type="text" defaultValue={day} readOnly />
+                        <input onClick={() => setShowCalendar(true)} required className='h-7 pl-2 w-[20%] border border-dark rounded-lg hover:border-secondary0Hover outline-secondary0Hover' placeholder='Mes' type="text" defaultValue={month} readOnly />
+                        <input onClick={() => setShowCalendar(true)} required className='h-7 pl-2 w-[20%] border border-dark rounded-lg hover:border-secondary0Hover outline-secondary0Hover' placeholder='Año' type="text" defaultValue={year} readOnly />
+                        <button onClick={() => setShowCalendar(!showCalendar)} className='w-[40px] h-[40px] cursor-pointer outline-none'>
+                            <img src={calendarIcon} alt="Seleccionar Fecha" />
                         </button>
+                        <button onClick={() => getDate(null)} className='w-[40px] h-[40px] cursor-pointer outline-none'>
+                            <img src={calendarIcon} alt="Seleccionar Fecha" />
+                        </button>
+                        {showCalendar && <DatePicker setSelectedDate={setSelectedDate} selectedDate={selectedDate} onChange={getDate} setShowCalendar={setShowCalendar} />}
                     </div>
                 </div>
                 <div className='flex w-[50%] justify-around'>
                     <button className='w-[100px] h-[35px] bg-secondary0 hover:bg-primary0 border border-dark text-disable rounded-lg'> Guardar </button>
-                    <button className='w-[100px] h-[35px] bg-secondary0 hover:bg-primary0 border border-dark text-disable rounded-lg'> Cancelar </button>
+                    <Link to={"/inventory"}>
+                        <button className='w-[100px] h-[35px] bg-secondary0 hover:bg-primary0 border border-dark text-disable rounded-lg'> Cancelar </button>
+                    </Link>
                 </div>
             </div>
             <div className='w-2/4 h-full flex items-center justify-center'>
-                <img className='w-[70%]' src={imgAddDevice} alt="Image" />
+                <input id='loadImg' className='hidden' type="file" accept='image/*' onChange={loadImage} />
+                <img className='w-[70%] cursor-pointer'
+                    onClick={clickLoadImg}
+                    src={selectedImage ? URL.createObjectURL(selectedImage) : imgAddDevice}
+                    alt="Carga imagen del dispositivo" />
             </div>
         </div>
     )
