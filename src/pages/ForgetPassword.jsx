87ba271ee13a3306792d9d3b88762e4f90f74366
JALZ <jannus.largo@correounivalle.edu.co>
2024-11-01 01:06:27 -0500

    Change redirection in signin for admin and change component sendEmail for popup alert, and remove component sendEmail

--- src/pages/ForgetPassword.jsx
+++ src/pages/ForgetPassword.jsx
@@ -1,28 +1,22 @@
-import { useState } from "react";
 import loginImg from "../assets/FondoLogin.png";
 import DigitarCorreo from "../components/DigitarCorreo";
-import SendEmail from "../components/SendEmail";
 import ChangeNewPassword from "../components/ChangeNewPassword";
 import { useParams } from "react-router-dom";
 export default function ForgetPassword() {
 
   const params = useParams();
   const token = params.token;
-  const [show, setShow] = useState(true);
 
   return (
     <main className="flex flex-col justify-center items-center h-screen w-screen">
       <img
         className="h-screen w-screen absolute top-0 left-0 -z-10"
         src={loginImg}
-        alt=""
       />
       {token.localeCompare("null") ? (
         <ChangeNewPassword token={token}/>
-      ) : show ? (
-       <DigitarCorreo setShow={setShow} show={show}/>
       ) : (
-       <SendEmail />
+       <DigitarCorreo />
       )}
     </main>
   );
