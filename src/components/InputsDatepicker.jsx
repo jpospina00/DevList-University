7b757e5bb2a0b9c770b6eb133b6c81b2645ccd9a
GustavoLS <106133669+GustavoLS1@users.noreply.github.com>
2024-11-17 01:59:07 -0500

    Create pop-up request devices

--- src/components/InputsDatepicker.jsx
+++ src/components/InputsDatepicker.jsx
@@ -1,19 +1,88 @@
-export default function InputsDatepicker({showCalendar, day, month, year, getDate}) {
-    return (
-        <div className='flex flex-col justify-around w-4/5 h-16'>
-            <label> Fecha </label>
-            <div className='flex w-full justify-between items-center relative'>
-                <input onClick={() => setShowCalendar(true)} required className='h-7 pl-2 w-[20%] border border-dark rounded-lg hover:border-secondary0Hover outline-secondary0Hover' placeholder='Dìa' type="text" defaultValue={day} readOnly />
-                <input onClick={() => setShowCalendar(true)} required className='h-7 pl-2 w-[20%] border border-dark rounded-lg hover:border-secondary0Hover outline-secondary0Hover' placeholder='Mes' type="text" defaultValue={month} readOnly />
-                <input onClick={() => setShowCalendar(true)} required className='h-7 pl-2 w-[20%] border border-dark rounded-lg hover:border-secondary0Hover outline-secondary0Hover' placeholder='Año' type="text" defaultValue={year} readOnly />
-                <button onClick={() => setShowCalendar(!showCalendar)} className='w-[40px] h-[40px] cursor-pointer outline-none'>
-                    <img src={calendarIcon} alt="Seleccionar Fecha" />
-                </button>
-                <button onClick={() => getDate(null)} className='w-[40px] h-[40px] cursor-pointer outline-none'>
-                    <img src={calendarIcon} alt="Seleccionar Fecha" />
-                </button>
-                {showCalendar && <DatePicker setSelectedDate={setSelectedDate} selectedDate={selectedDate} onChange={getDate} setShowCalendar={setShowCalendar} />}
-            </div>
-        </div>
-    )
+import calendarIcon from "../assets/calendarIcon.svg";
+import DatePicker from "./DatePicker";
+import clearIcon from "../assets/clearIcon.svg";
+
+export default function InputsDatepicker({
+  showCalendar,
+  day,
+  month,
+  year,
+  getDate,
+  setSelectedDate,
+  selectedDate,
+  setShowCalendar,
+}) {
+  return (
+    <div className="flex flex-col justify-around w-4/5 h-16 gap-2 font-montserrat">
+      <label> Fecha </label>
+      <div className="flex w-full justify-between items-center relative">
+        <input
+          onClick={() => setShowCalendar(true)}
+          required
+          className="p-2 w-[20%] border border-dark rounded-lg hover:border-secondary0Hover outline-secondary0Hover"
+          placeholder="Día"
+          type="text"
+          defaultValue={day}
+          readOnly
+        />
+        <input
+          onClick={() => setShowCalendar(true)}
+          required
+          className="p-2 w-[20%] border border-dark rounded-lg hover:border-secondary0Hover outline-secondary0Hover"
+          placeholder="Mes"
+          type="text"
+          defaultValue={month}
+          readOnly
+        />
+        <input
+          onClick={() => setShowCalendar(true)}
+          required
+          className="p-2 w-[20%] border border-dark rounded-lg hover:border-secondary0Hover outline-secondary0Hover"
+          placeholder="Año"
+          type="text"
+          defaultValue={year}
+          readOnly
+        />
+        <button
+          onClick={() => setShowCalendar(!showCalendar)}
+          className="w-[40px] h-[40px] cursor-pointer outline-none"
+        >
+          <img
+            className="w-full h-full"
+            src={calendarIcon}
+            alt="Seleccionar Fecha"
+          />
+        </button>
+        <button
+          onClick={() => getDate(null)}
+          title="Add New"
+          className="group cursor-pointer outline-none rotate-45 duration-300"
+        >
+          <svg
+            width="35px"
+            height="35px"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            className="stroke-secondary2 fill-none group-hover:stroke-[#c20000] group-hover:duration-0 duration-300"
+          >
+            <path
+              d="M12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22Z"
+              stroke-width="1.5"
+            ></path>
+            <path d="M8 12H16" stroke-width="1.5"></path>
+            <path d="M12 16V8" stroke-width="1.5"></path>
+          </svg>
+                 
+        </button>
+        {showCalendar && (
+          <DatePicker
+            setSelectedDate={setSelectedDate}
+            selectedDate={selectedDate}
+            onChange={getDate}
+            setShowCalendar={setShowCalendar}
+          />
+        )}
+      </div>
+    </div>
+  );
 }
--- src/components/PopupRequest.jsx
+++ src/components/PopupRequest.jsx
@@ -1,17 +1,100 @@
-import DatePicker from "./DatePicker";
+import { useEffect, useState } from "react";
+import InputsDatepicker from "./InputsDatepicker";
+import DropDown from "./Dropdown";
+
 
 export default function PopupRequest() {
+  const [showCalendar, setShowCalendar] = useState(false);
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [day, setDay] = useState("");
+  const [year, setYear] = useState("");
+  const [month, setMonth] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("");
+  const [selectedOptionName, setSelectedOptionName] = useState("");
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    generateHours();
+    console.log(list);
+  }, []);
+
+  const generateHours = () => {
+    let initHour = 7;
+    let endHour = 22;
+    let listHours = [];
+
+    for (let i = initHour; i <= endHour; i++) {
+      listHours.push({ id: `${i}:00`, name: `${i}:00` });
+      listHours.push({ id: `${i}:30`, name: `${i}:30` });
+    }
+    setList(listHours);
+  };
+
+  const getDate = (date) => {
+    if (date == null) {
+      setYear("");
+      setMonth("");
+      setDay("");
+    } else {
+      setYear(date.getFullYear());
+      setMonth(date.getMonth() + 1);
+      setDay(date.getDate());
+    }
+  };
+
+  const handleOptionChange = (event) => {
+    setSelectedOptionName(event.target.id);
+    setSelectedOption(event.target.value);
+    setIsOpen(false); // Cierra el dropdown al seleccionar una opción
+  };
+
   return (
-    <div className="fixed w-screen h-screen bg-dark flex items-center justify-center bg-opacity-50 z-50 top-0">
-      <div className="flex flex-col bg-disable w-[478px] h-[357px] items-center justify-center gap-5 rounded-3xl">
-        <h3>Fecha programada:</h3>
-        <div className="">
-            <DatePicker/>
-          <h3>hola</h3>
+    <div className="fixed w-screen h-screen bg-dark flex items-center justify-center bg-opacity-50 z-40 top-0">
+      <div className="flex flex-col bg-disable w-[35%] h-[40%] items-center justify-center gap-10 rounded-3xl">
+        <h3 className="font-montserrat font-bold text-2xl w-[90%] ">
+          Fecha programada:
+        </h3>
+        <div className="w-[90%] flex gap-5 justify-between ">
+          <InputsDatepicker
+            day={day}
+            month={month}
+            year={year}
+            getDate={getDate}
+            selectedDate={selectedDate}
+            setSelectedDate={setSelectedDate}
+            showCalendar={showCalendar}
+            setShowCalendar={setShowCalendar}
+          />
+          <div className="flex flex-col gap-2 w-[30%] font-montserrat">
+            <label>hora</label>
+            <DropDown
+              handleOptionChange={handleOptionChange}
+              isOpen={isOpen}
+              list={list}
+              selectedOption={selectedOption}
+              selectedOptionName={selectedOptionName}
+              setIsOpen={setIsOpen}
+            />
+          </div>
+          <div className="flex flex-col gap-2 items-end w-[25%] ">
+            <label className="w-full font-montserrat">
+                horas a usar
+            </label>
+            <input
+              required
+              className="p-2 w-full border border-dark rounded-lg hover:border-secondary0Hover outline-secondary0Hover"
+              type="number"
+            />
+          </div>
         </div>
-        <div className="flex justify-center gap-7 w-full">
-            <button className="w-1/5 h-[30px] border border-secondary0 text-secondary0 rounded-lg hover:bg-secondary0Hover hover:text-disable">Cancelar</button>
-            <button className="w-1/5 h-[30px] border border-secondary0 text-secondary0 rounded-lg hover:bg-secondary0Hover hover:text-disable">Aceptar</button>
+        <div className="flex justify-center gap-7 w-full h-[10%] font-montserrat">
+          <button className="w-1/5 h-full border border-secondary0 text-secondary0 rounded-lg hover:bg-secondary0Hover hover:text-disable">
+            Cancelar
+          </button>
+          <button className="w-1/5 h-full border border-secondary0 text-secondary0 rounded-lg hover:bg-secondary0Hover hover:text-disable">
+            Aceptar
+          </button>
         </div>
       </div>
     </div>
