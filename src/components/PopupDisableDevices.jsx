8b161e50f8148776cad090b99a345e0fca4ef5a1
GustavoLS <106133669+GustavoLS1@users.noreply.github.com>
2024-11-26 22:45:27 -0500

    Update input Active/Disable devices

--- src/components/PopupDisableDevices.jsx
+++ src/components/PopupDisableDevices.jsx
@@ -4,21 +4,22 @@ export default function PopupDisableDevices({ setShow, handleActiveChange }) {
   return (
     <div className="fixed w-screen h-screen bg-dark flex items-center justify-center bg-opacity-50 z-50 top-0 left-0">
       <div className="flex flex-col bg-disable w-[478px] h-[400px] items-center justify-center gap-5 rounded-3xl">
-        <div className="flex flex-col justify-around w-4/5 relative gap-1">
+        <div className="flex flex-col justify-around w-4/5 relative gap-3">
           <h1 className="font-montserrat font-bold text-[24px]">Motivo</h1>
           <textarea
             placeholder="Escriba su observación"
-            className="text-start h-28 max-h-32 min-h-10 p-2 border border-dark rounded"
+            className=" h-28 max-h-32 min-h-10 p-3 border border-dark rounded"
           ></textarea>
-         <div className="w-[352px] h-[58px] relative">
+         <div className="w-[383px] h-[58px] relative">
             <input
               type="text"
-              className="peer text-start h-20 max-h-10 p-2 w-full pb-12 border rounded mt-3 border-dark pt-6  pr-4 "
+              // className="peer text-start h-20 max-h-10 p-2 w-full pb-12 border rounded mt-3 border-dark pt-6  pr-4 "
+              className="peer w-full p-4 pt-6  pr-4 bg-inherit border-2 rounded outline-none transition disabled:opacity-70 disabled:cursor-not-allowed border-gray-500 focus:border-purple-500"
               placeholder=""
               name="firma"
             />
             <label
-              className="absolute text-gray-500 text-base duration-150 transform -translate-y-3 top-8 z-10 origin-[0] right-[85%] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 peer-focus:text-purple-500"
+              className="absolute text-base transform top-1 z-10 origin-[0] left-[3.5%] opacity-50"
               htmlFor="firma"
             >
               Firma
