be20b9bb304d15960ceeda8ec54a844ac999f66c
JALZ <jannus.largo@correounivalle.edu.co>
2024-12-06 23:48:15 -0500

    update

--- src/components/PopupRequest.jsx
+++ src/components/PopupRequest.jsx
@@ -1,114 +1,156 @@
 import { useEffect, useState } from "react";
 import InputsDatepicker from "./InputsDatepicker";
 import DropDown from "./Dropdown";
+import axios from "axios";
+import Api from "../Api";
+import Headers from "../Headers";
+import Swal from "sweetalert2";
 
 
-export default function PopupRequest({ setShowRequest }) {
-  
-  const [showCalendar, setShowCalendar] = useState(false);
-  const [selectedDate, setSelectedDate] = useState(null);
-  const [day, setDay] = useState("");
-  const [year, setYear] = useState("");
-  const [month, setMonth] = useState("");
-  const [isOpen, setIsOpen] = useState(false);
-  const [selectedOption, setSelectedOption] = useState("");
-  const [selectedOptionName, setSelectedOptionName] = useState("");
-  const [list, setList] = useState([]);
-  const [value, setValue] = useState(0);
+export default function PopupRequest({ setShowRequest, devices }) {
 
-  const handleChange = (e) => {
-    const newValue = Number(e.target.value);
-    if (newValue >= 0) {
-      setValue(newValue);
-    }
-  };
+	const [showCalendar, setShowCalendar] = useState(false);
+	const [selectedDate, setSelectedDate] = useState(null);
+	const [day, setDay] = useState("");
+	const [year, setYear] = useState("");
+	const [month, setMonth] = useState("");
+	const [isOpen, setIsOpen] = useState(false);
+	const [selectedOption, setSelectedOption] = useState("");
+	const [selectedOptionName, setSelectedOptionName] = useState("");
+	const [list, setList] = useState([]);
+	const [value, setValue] = useState(0);
 
-  useEffect(() => {
-    generateHours();
-    console.log(list);
-  }, []);
+	const handleChange = (e) => {
+		const newValue = Number(e.target.value);
+		if (newValue >= 0) {
+			setValue(newValue);
+		}
+	};
 
-  const generateHours = () => {
-    let initHour = 7;
-    let endHour = 22;
-    let listHours = [];
+	useEffect(() => {
+		generateHours();
+		console.log(list);
+	}, []);
 
-    for (let i = initHour; i <= endHour; i++) {
-      listHours.push({ id: `${i}:00`, name: `${i}:00` });
-      listHours.push({ id: `${i}:30`, name: `${i}:30` });
-    }
-    setList(listHours);
-  };
+	const generateHours = () => {
+		let initHour = 7;
+		let endHour = 22;
+		let listHours = [];
 
-  const getDate = (date) => {
-    if (date == null) {
-      setYear("");
-      setMonth("");
-      setDay("");
-    } else {
-      setYear(date.getFullYear());
-      setMonth(date.getMonth() + 1);
-      setDay(date.getDate());
-    }
-  };
+		for (let i = initHour; i <= endHour; i++) {
+			listHours.push({ id: `${i}:00`, name: `${i}:00` });
+			listHours.push({ id: `${i}:30`, name: `${i}:30` });
+		}
+		setList(listHours);
+	};
 
-  const handleOptionChange = (event) => {
-    setSelectedOptionName(event.target.id);
-    setSelectedOption(event.target.value);
-    setIsOpen(false); // Cierra el dropdown al seleccionar una opción
-  };
+	const getDate = (date) => {
+		if (date == null) {
+			setYear("");
+			setMonth("");
+			setDay("");
+		} else {
+			setYear(date.getFullYear());
+			setMonth(date.getMonth() + 1);
+			setDay(date.getDate());
+		}
+	};
 
-  return (
-    <div className="fixed w-screen h-screen bg-dark flex items-center justify-center bg-opacity-50 z-40 top-0">
-      <div className="flex flex-col bg-disable w-[35%] h-[40%] items-center justify-center gap-10 rounded-3xl">
-        <h3 className="font-montserrat font-bold text-2xl w-[90%] ">
-          Fecha programada:
-        </h3>
-        <div className="w-[90%] flex gap-5 justify-between ">
-          <InputsDatepicker
-            day={day}
-            month={month}
-            year={year}
-            getDate={getDate}
-            selectedDate={selectedDate}
-            setSelectedDate={setSelectedDate}
-            showCalendar={showCalendar}
-            setShowCalendar={setShowCalendar}
-          />
-          <div className="flex flex-col gap-2 w-[30%] font-montserrat">
-            <label className="text-[80%]">hora</label>
-            <DropDown
-              handleOptionChange={handleOptionChange}
-              isOpen={isOpen}
-              list={list}
-              selectedOption={selectedOption}
-              selectedOptionName={selectedOptionName}
-              setIsOpen={setIsOpen}
-            />
-          </div>
-          <div className="flex flex-col gap-2 items-end w-[25%] ">
-            <label className="w-full font-montserrat text-[80%]">
-              horas a usar
-            </label>
-            <input
-              required
-              value={value}
-              onChange={handleChange}
-              className="p-2 w-full border border-dark rounded-lg hover:border-secondary0Hover outline-secondary0Hover"
-              type="number"
-            />
-          </div>
-        </div>
-        <div className="flex justify-center gap-7 w-full h-[10%] font-montserrat">
-          <button onClick={() => setShowRequest(false)}
-            className="w-1/5 h-full border border-secondary0 text-secondary0 rounded-lg hover:bg-secondary0Hover hover:text-disable">
-            Cancelar
-          </button>
-          <button className="w-1/5 h-full border border-secondary0 text-secondary0 rounded-lg hover:bg-secondary0Hover hover:text-disable">
-            Aceptar
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+	const handleOptionChange = (event) => {
+		setSelectedOptionName(event.target.id);
+		setSelectedOption(event.target.value);
+		setIsOpen(false); // Cierra el dropdown al seleccionar una opción
+	};
+
+	const getData = () => {
+		if (devices.length > 0) {
+			console.log(day, month, year, selectedOption, value, devices);
+			let data = {
+				request: {
+					requestDate: `${year}-${month}-${day}`,
+					requestTime: `${selectedOption}:00`,
+					hoursRequested: value,
+					device: devices.map(device => {
+						return {
+							deviceId: device.deviceId,
+							quantity: device.quantity,
+						}
+					})
+				}
+			}
+			console.log(data);
+			axios.post(`${Api}request/pending-devices/${devices[0].requestId}`, data, Headers('application/json')).then(res => {
+				Swal.fire({
+					position: "center",
+					icon: "success",
+					title: `!!Solicitud enviada con éxito¡¡`,
+					timer: 1500
+				});
+			}).catch(err => {
+				console.log(err);
+			})
+		} else {
+			Swal.fire({
+				icon: "error",
+				title: "Oops...",
+				text: "!No hay dispositivos¡"
+			});
+		}
+
+	}
+
+	return (
+		<div className="fixed w-screen h-screen bg-dark flex items-center justify-center bg-opacity-50 z-40 top-0">
+			<div className="flex flex-col bg-disable w-[40%] h-[40%] items-center justify-center gap-10 rounded-3xl">
+				<h3 className="font-montserrat font-bold text-2xl w-[90%] ">
+					Fecha programada:
+				</h3>
+				<div className="w-[90%] flex gap-5 justify-between ">
+					<InputsDatepicker
+						day={day}
+						month={month}
+						year={year}
+						getDate={getDate}
+						selectedDate={selectedDate}
+						setSelectedDate={setSelectedDate}
+						showCalendar={showCalendar}
+						setShowCalendar={setShowCalendar}
+					/>
+					<div className="flex flex-col gap-2 w-[30%] font-montserrat">
+						<label className="text-[80%]">hora</label>
+						<DropDown
+							handleOptionChange={handleOptionChange}
+							isOpen={isOpen}
+							list={list}
+							selectedOption={selectedOption}
+							selectedOptionName={selectedOptionName}
+							setIsOpen={setIsOpen}
+						/>
+					</div>
+					<div className="flex flex-col gap-2 items-end w-[25%] ">
+						<label className="w-full font-montserrat text-[80%]">
+							horas a usar
+						</label>
+						<input
+							required
+							value={value}
+							onChange={handleChange}
+							className="p-2 w-full border border-dark rounded-lg hover:border-secondary0Hover outline-secondary0Hover"
+							type="number"
+						/>
+					</div>
+				</div>
+				<div className="flex justify-center gap-7 w-full h-[10%] font-montserrat">
+					<button onClick={() => setShowRequest(false)}
+						className="w-1/5 h-full border border-secondary0 text-secondary0 rounded-lg hover:bg-secondary0Hover hover:text-disable">
+						Cancelar
+					</button>
+					<button onClick={getData}
+						className="w-1/5 h-full border border-secondary0 text-secondary0 rounded-lg hover:bg-secondary0Hover hover:text-disable">
+						Aceptar
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
--- src/pages/RequestDevices.jsx
+++ src/pages/RequestDevices.jsx
@@ -12,11 +12,18 @@ export default function RequestDevices() {
     const [show, setShow] = useState(false);
     const [showRequest, setShowRequest] = useState(false);
     const [devices, setDevices] = useState([]);
+    const [count, setCount] = useState(0);
+    const [message, setMessage] = useState("");
 
     useEffect(() => {
         axios.get(`${Api}request/waited`, Headers('application/json')).then(res => {
-            console.log(res.data);
-            setDevices(res.data);
+            console.log(res.data.message);
+            if (res.data.length > 0) {
+                setDevices(res.data);
+                setCount(res.data.length);
+            } else {
+                setMessage(res.data.message);
+            }
         }).catch(err => {
             console.log(err);
         })
@@ -28,26 +35,31 @@ export default function RequestDevices() {
 
     return (
         <>
-            {showRequest && <PopupRequest setShowRequest={setShowRequest} />}
+            {showRequest && <PopupRequest setShowRequest={setShowRequest} devices={devices} />}
             {show && <PopupDelete setShow={setShow} show={show} />}
             <div className="w-full h-full pt-5 pb-5 flex flex-col items-center">
                 <div className="flex w-[90%] justify-between pb-5">
                     <h1 className="font-montserrat font-semibold text-4xl"> Equipos a solicitar </h1>
                     <div className="flex w-3/6 justify-end gap-5">
-                        <Link to={'/history'} className="flex items-center justify-center w-[135px] h-[35px] border border-secondary0 text-secondary0 rounded-lg hover:bg-secondary0Hover hover:text-disable"> 
-                            Historial 
+                        <Link to={'/history'} className="flex items-center justify-center w-[135px] h-[35px] border border-secondary0 text-secondary0 rounded-lg hover:bg-secondary0Hover hover:text-disable">
+                            Historial
                         </Link>
                     </div>
                 </div>
                 <hr className="border w-[90%] border-secondary0" />
                 <div className="w-[90%] h-full flex flex-col pt-5">
                     <h2 className="font-montserrat font-bold text-2xl pb-5"> Dispositivos </h2>
-                    <form  className="w-full h-[70%] overflow-scroll flex flex-col gap-5">
-                        {devices.map(device => <CardDeviceRequest key={device.devdeviceId} device={device} setShow={setShow} />)}
+                    <form className="w-full h-[70%] overflow-scroll flex flex-col gap-5">
+                        {
+                            devices.length > 0 ? devices.map(device => <CardDeviceRequest key={device.devdeviceId} device={device} setShow={setShow} />) :
+                                <div className="w-full h-full flex items-center justify-center font-montserrat font-bold text-2xl">
+                                    {message}
+                                </div>
+                        }
                     </form>
                     <div className="flex justify-between h-[10%] items-center">
                         <h2 className="font-montserrat font-bold text-2xl">
-                            Total de dispositivos 4
+                            Total de dispositivos {count}
                         </h2>
                         <button onClick={request}
                             className="w-[145px] h-[30px] border border-secondary0 text-secondary0 rounded-lg hover:bg-secondary0Hover hover:text-disable">
